396c4147be166261ec984fe41356f4bd19f642a9
otgo <otgo976@gmail.com>
2022-09-03 21:44:12 +0600

    added cahing examples

--- src/main/java/criteriaAPI/RunnerFetch10.java
+++ src/main/java/criteriaAPI/RunnerFetch10.java
@@ -7,8 +7,10 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Random;
@@ -37,48 +39,65 @@ public class RunnerFetch10 {
 
 
         //How to use parameter in HQL
-        Long studentId =10L;
-        Query query = session.createQuery("FROM Student10 s where s.id=: id");
-        query.setParameter("id", studentId);
-        List<Student10> resultList2 = query.getResultList();
-        resultList2.forEach(System.out::println);
+//        Long studentId =10L;
+//        Query query = session.createQuery("FROM Student10 s where s.id=: id");
+//        query.setParameter("id", studentId);
+//        List<Student10> resultList2 = query.getResultList();
+//        resultList2.forEach(System.out::println);
+//
+//        String hqlQuery2 = "Select sum(mathGrade) from Student10 where mathGrade>90";
+//        Long sumMathGrade  = (Long) session.createQuery(hqlQuery2).uniqueResult();
+//
+//        System.out.println("Total math Grade: "+sumMathGrade );
+//
+//        int mathGrade  = 90;
+//        String hqlQuery3 = "Select avg(mathGrade) from Student10 where mathGrade>:mathGrade";
+//        Query query2 = session.createQuery(hqlQuery3);
+//        query2.setParameter("mathGrade", mathGrade);
+//        Double avgMathGrade  = (Double) query2.uniqueResult();
+//
+//        System.out.println("AVG math Grade: "+avgMathGrade );
+//
+//        Student10 student = session.get(Student10.class, 1L);
+//        student.setName("Updated "+student.getName());
+//        session.update(student);
+//
+//        String hqlQuery4="UPDATE Student10 s SET s.name ='Updated with HQL' where id =11";
+//        int  numOfRecords = session.createQuery(hqlQuery4).executeUpdate();
+//        System.out.println("Number of effected row count: "+numOfRecords);
+//
+//        String hqlQuery5 = "FROM Student10 s WHERE s.mathGrade <50";
+//        List<Student10> resultList3 =  session.createQuery(hqlQuery5).getResultList();
+//        for (Student10 s:resultList3){
+//            s.setMathGrade(50);
+//            session.update(s);
+//        }
+//        CriteriaBuilder cb = session.getCriteriaBuilder();
+//        CriteriaQuery<Student10> criteriaQuery =  cb.createQuery(Student10.class);
+//        Root<Student10>   root = criteriaQuery.from(Student10.class);
+//
+//        //criteriaQuery.select(root);
+//        //criteriaQuery.select(root).where(cb.equal(root.get("name"), "Name 16"));
+//
+//        Predicate predicateForName = cb.equal((root.get("name")), "Name 10");
+//        Predicate predicateForMathGradeGt80 =cb.greaterThan(root.get("mathGrade"), 80);
+//        Predicate predicateStd = cb.or(predicateForName, predicateForMathGradeGt80);
+//        criteriaQuery.where(predicateStd);
+//
+//        Query<Student10> query1 = session.createQuery(criteriaQuery);
+//        List<Student10> resultList7 = query1.getResultList();
+//        resultList7.forEach(System.out::println);
 
-        String hqlQuery2 = "Select sum(mathGrade) from Student10 where mathGrade>90";
-        Long sumMathGrade  = (Long) session.createQuery(hqlQuery2).uniqueResult();
+        CriteriaBuilder cb2 = session.getCriteriaBuilder();
+        CriteriaQuery<Integer> criteriaQuery2 = cb2.createQuery(Integer.class);
+        Root<Student10> root2 = criteriaQuery2.from(Student10.class);
+        criteriaQuery2.select(cb2.sum(root2.get("mathGrade")));
+        TypedQuery<Integer> typedQuery = session.createQuery(criteriaQuery2);
 
-        System.out.println("Total math Grade: "+sumMathGrade );
+        Integer totalMathGrade = typedQuery.getSingleResult();
+        System.out.println("Total MathGrade : " +totalMathGrade);
 
-        int mathGrade  = 90;
-        String hqlQuery3 = "Select avg(mathGrade) from Student10 where mathGrade>:mathGrade";
-        Query query2 = session.createQuery(hqlQuery3);
-        query2.setParameter("mathGrade", mathGrade);
-        Double avgMathGrade  = (Double) query2.uniqueResult();
 
-        System.out.println("AVG math Grade: "+avgMathGrade );
-
-        Student10 student = session.get(Student10.class, 1L);
-        student.setName("Updated "+student.getName());
-        session.update(student);
-
-        String hqlQuery4="UPDATE Student10 s SET s.name ='Updated with HQL' where id =11";
-        int  numOfRecords = session.createQuery(hqlQuery4).executeUpdate();
-        System.out.println("Number of effected row count: "+numOfRecords);
-
-        String hqlQuery5 = "FROM Student10 s WHERE s.mathGrade <50";
-        List<Student10> resultList3 =  session.createQuery(hqlQuery5).getResultList();
-        for (Student10 s:resultList3){
-            s.setMathGrade(50);
-            session.update(s);
-        }
-        CriteriaBuilder cb = session.getCriteriaBuilder();
-        CriteriaQuery<Student10> criteriaQuery =  cb.createQuery(Student10.class);
-        Root<Student10>   root = criteriaQuery.from(Student10.class);
-
-        //criteriaQuery.select(root);
-        criteriaQuery.select(root).where(cb.equal(root.get("name"), "Name 16"));
-        Query<Student10> query1 = session.createQuery(criteriaQuery);
-        List<Student10> resultList7 = query1.getResultList();
-        resultList7.forEach(System.out::println);
 
         //it makes data to write into database, makes data permanent
         tx.commit();
